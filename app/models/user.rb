dd9ee020263849e461ac9361dff9557743bff517
Bret Doucette <doucette.bret@gmail.com>
2016-11-15 09:25:50 -0700

    Add single inheritance in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  def self.types
+    %w(Admin Teacher Student)
+  end
+
   validates :first_name, :last_name, :email,
     presence: true
 
@@ -11,4 +15,12 @@ class User < ActiveRecord::Base
 
   validates :password_confirmation,
     presence: true
+
+  scope :admins, -> { where(type: 'Admin') }
+  scope :teachers, -> { where(type: 'Teacher') }
+  scope :students, -> { where(type: 'Student') }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
