6c9717addb5cd1d61070846e5d75da2f9a3818c0
Bret Doucette <doucette.bret@gmail.com>
2016-11-15 17:05:12 -0700

    Set up relation between students and courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,9 @@ class Course < ActiveRecord::Base
   belongs_to :semester
   belongs_to :teacher, class_name: "User"
 
+  has_many :student_courses
+  has_many :students, through: :student_courses
+
   validates :name, :description,
     presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < User
-
+  has_many :student_courses
+  has_many :courses, through: :student_courses
 end
