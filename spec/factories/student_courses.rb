86273588ef4b3e5ed228aeba12cef781c3c3a2a7
Bret Doucette <doucette.bret@gmail.com>
2016-11-15 17:56:11 -0700

    Add student_course spec

--- spec/factories/student_courses.rb
+++ spec/factories/student_courses.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :student_course do
-    student nil
-    course nil
+    student { FactoryGirl.create(:student) }
+    course { FactoryGirl.create(:course) }
+    grade { [0,1,2,3,4].sample }
   end
 end
--- spec/models/student_course_spec.rb
+++ spec/models/student_course_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe StudentCourse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe StudentCourse do
+  subject { create(:student_course) }
+
+  it { should be_valid }
+
+  it { should respond_to(:student_id) }
+  it { should respond_to(:course_id) }
+  it { should respond_to(:grade) }
+
+  it { should validate_presence_of(:student_id) }
+  it { should validate_presence_of(:course_id) }
+  it { should validate_presence_of(:grade) }
+
+  it { should belong_to(:student) }
+  it { should belong_to(:course) }
+
+  it { should validate_uniqueness_of(:student_id).scoped_to(:course_id) }
+
+  it { should validate_numericality_of(:grade) }
 end
