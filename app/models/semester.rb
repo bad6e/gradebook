5b7d011052504e0a926644d2ba4a7d05aa903e5a
Bret Doucette <doucette.bret@gmail.com>
2016-11-18 16:14:38 -0700

    Add associations to courses

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -7,6 +7,8 @@ class Semester < ActiveRecord::Base
 
   validate :verify_end_date_is_after_begin_date
 
+  has_many :courses
+
   def verify_end_date_is_after_begin_date
     if  (end_date && begin_date) && end_date <= begin_date
       errors[:base] << "Semester begin date must be before end date"
