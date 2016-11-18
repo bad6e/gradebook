d4450d3f39cced6d69f70788bb0cfb6069e449b1
Bret Doucette <doucette.bret@gmail.com>
2016-11-18 16:02:34 -0700

    Add additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,33 +31,50 @@ student  = Student.create!({email: "student@gmail.com",
                             last_name: "Bagins"
                            })
 
-60.times do
-  Student.create!({email: (0...8).map { (65 + rand(26)).chr }.join + "@gmail.com",
+75.times do
+  student = Student.create!({email: (0...8).map { (65 + rand(26)).chr }.join + "@gmail.com",
                    password: "12345678",
                    password_confirmation: "12345678",
                    first_name: Faker::Name.first_name,
                    last_name: Faker::Name.last_name
                   })
+  puts "Created #{student.full_name} - Student"
+end
+
+3.times do
+  teacher = Teacher.create!({email: (0...8).map { (65 + rand(26)).chr }.join + "@gmail.com",
+                   password: "12345678",
+                   password_confirmation: "12345678",
+                   first_name: Faker::Name.first_name,
+                   last_name: Faker::Name.last_name
+                  })
+  puts "Created #{teacher.full_name} - Teacher"
 end
 
 month_count = 15
 5.times do
-  Semester.create!({begin_date: Date.today - month_count.months,
+  semester = Semester.create!({begin_date: Date.today - month_count.months,
                     end_date: Date.today - ((month_count - 3).months)
                   })
   month_count = month_count - 3
+  puts "Created semester - #{semester.id}"
 end
 
-15.times do
-  Course.create!({name: Faker::Company.catch_phrase,
+30.times do
+  course = Course.create!({name: Faker::Company.catch_phrase,
                   description: Faker::Company.bs,
                   teacher_id: Teacher.limit(1).order("RANDOM()").first.id,
                   semester_id: Semester.limit(1).order("RANDOM()").first.id
                 })
+  puts "Created Course: #{course.name}"
 end
 
 Student.all.each do |s|
-  StudentCourse.create!(student_id: s.id,
-                        course_id: Course.limit(1).order("RANDOM()").first.id,
-                        grade: [0,1,2,3,4].sample)
+  course_ids = Course.limit([2,3,4,5,6,7,8,9,10].sample).order("RANDOM()").pluck(:id)
+  course_ids.each do |id|
+    StudentCourse.create!(student_id: s.id,
+                          course_id: id,
+                          grade: [0,1,2,3,4].sample)
+    puts "Associated course with student: #{s.full_name}"
+  end
 end
