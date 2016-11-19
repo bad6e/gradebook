323f33f2d8d8c38f226c3906893922f6d5a41ecc
Bret Doucette <doucette.bret@gmail.com>
2016-11-19 11:40:53 -0700

    Fix bug

--- spec/factories/semesters.rb
+++ spec/factories/semesters.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :semester do
-    begin_date { Date.today + Random.rand(1111).days}
-    end_date { Date.today + Random.rand(1111).months }
+    begin_date { Date.today + Random.rand(1111111111).seconds }
+    end_date { Date.today + Random.rand(1111111111).days }
   end
 end
