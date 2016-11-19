d93f16eb0b0368d470f40c06c04f9c74e99fd659
Bret Doucette <doucette.bret@gmail.com>
2016-11-18 17:54:48 -0700

    Add admin controller

---
+++ app/controllers/api/v1/admins_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::AdminsController < ApplicationController
+  respond_to :json
+  # before_action :verify_user_in_params_matches_current_user, only: [:show]
+
+  def show
+    @grade = Course.preload(:student_courses).find(admin_params[:id])
+    respond_with @grade.student_courses.pluck(:grade)
+  end
+
+  private
+
+    def admin_params
+      params.permit(:id)
+    end
+
+    def verify_user_in_params_matches_current_user
+      if current_user.id != admin_params[:id].to_i
+         render status: 401, json: {
+          error: "Students can only see their own schedule and grades."
+        }
+      end
+    end
+end
