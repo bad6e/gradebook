7215d3263d334979321f1eb5c7da67da41a5be1e
Bret Doucette <doucette.bret@gmail.com>
2016-11-21 14:15:44 -0700

    Add round for grade

--- app/assets/javascripts/components/courses/students.jsx
+++ app/assets/javascripts/components/courses/students.jsx
@@ -2,7 +2,7 @@ var Students = React.createClass({
   renderStudents: function(student) {
     const std = student.students;
     return (
-      <li>{std.fullName} | Grade: {std.grade}</li>
+      <li>{std.fullName} | Grade: {std.grade.toFixed(2)}</li>
     )
   },
 
